452c77b3b96abab4981a4b88d93891e002568c6f
Jorge Esquiva <jorgeesquivait@gmail.com>
2024-05-02 21:24:14 +0200

    actualizarClicks

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,6 @@ function Index() {
     axios
       .get(`${apiUrl}`)
       .then((response) => {
-        console.log(response.data);
         setShortUrls(response.data);
       })
       .catch((error) => {
@@ -38,10 +37,11 @@ function Index() {
     event.preventDefault();
     axios
       .post(`${apiUrl}/shortUrls`, {
-        fullUrl,
+        fullUrl, // Asegúrate de que estas variables estén definidas en el contexto o sean recogidas del estado/formulario
         customShortId,
       })
       .then((response) => {
+        // Lógica para manejar la respuesta, por ejemplo actualizar el estado con la nueva URL corta
         setShortUrls([...shortUrls, response.data]);
         console.log("URL Shortened:", response.data);
       })
@@ -80,31 +80,17 @@ function Index() {
           <tr>
             <th>Full URL</th>
             <th>Short URL</th>
-            <th>CLicks</th>
           </tr>
         </thead>
         <tbody>
           {shortUrls.map((shortUrl) => (
             <tr key={shortUrl._id}>
               <td>
-                <a href={shortUrl.full} target="_blank">
-                  {shortUrl.full}
-                </a>
+                <a href={shortUrl.full}>{shortUrl.full}</a>
               </td>
               <td>
-                <a
-                  href={`${apiUrl}/${shortUrl.short}`}
-                  target="_blank"
-                  onClick={() => {
-                    window.setTimeout(() => {
-                      window.location.reload();
-                    }, 100);
-                  }}
-                >
-                  {shortUrl.short}
-                </a>
+                <a href={`${apiUrl}/${shortUrl.short}`}>{shortUrl.short}</a>
               </td>
-              <td>{shortUrl.clicks}</td>
               <td>
                 <button
                   onClick={() => deleteUrl(shortUrl._id)}
